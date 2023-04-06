b53e6d0d68eea5c042b7bc29b7265e4623a0276b
Thomas Camminady <0milieux_member@icloud.com>
2023-04-06 08:46:12 +0200

    Update.

--- app.py
+++ app.py
@@ -1,43 +1,57 @@
-import pandas as pd
 import streamlit as st
 
 
 def parse(input_string: str) -> list[str]:
-    return [s.strip() for s in input_string.split("\n")]
+    parsed_input = []
+    for s in input_string.split("\n"):
+        s = s.strip()
+        if s == "":
+            continue
+        if s[-1] == "," or s[-1] == ";":
+            s = s[:-1]
+        parsed_input.append(s)
+    return parsed_input
 
 
 def main():
     st.write("# XLOOKUP App")
-
+    height = 400
     col1, col2, col3 = st.columns(3)
     with col1:
-        reference_names = st.text_input("Reference names go here.")
+        reference_names = st.text_area("Reference names go here.", height=height)
     with col2:
-        reference_ids = st.text_input("Values for those reference names go here.")
+        reference_ids = st.text_area(
+            "Values for those reference go here.", height=height
+        )
     with col3:
-        lookup_names = st.text_input(
-            "Names for which you want to look up information go here."
-        )
+        lookup_names = st.text_area("Names to xlookup.", height=height)
+    not_found_value = st.text_input("What do use when not present?", "#NAME?")
+    if st.button("XLOOKUP"):
+        list_reference_names = parse(reference_names)
+        list_reference_ids = parse(reference_ids)
+        list_lookup_names = parse(lookup_names)
+        if len(list_reference_ids) != len(list_reference_names):
+            st.warning(
+                "The Names and Values fields need to have the same number of inputs. Not continuing."
+            )
+        else:
+            reference_dict = dict(zip(list_reference_names, list_reference_ids))
+            list_lookup_ids = [
+                reference_dict[name] if name in reference_dict else not_found_value
+                for name in list_lookup_names
+            ]
 
-    list_reference_names = parse(reference_names)
-    list_reference_ids = parse(reference_ids)
-    if len(list_reference_ids) != len(list_reference_names):
-        st.warning(
-            "The Names and Values fields need to have the same number of inputs. Not continuing."
-        )
-    else:
-        reference_dict = dict(zip(list_reference_names, list_reference_ids))
-        lookup_ids = [
-            reference_dict[name] if name in reference_dict else ""
-            for name in lookup_names
-        ]
-
-        df = pd.DataFrame({"Names": lookup_names, "Value": lookup_ids})
-        col4, col5 = st.columns(2)
-        with col4:
-            st.write(df)
-        with col5:
-            st.write(lookup_ids)
+            col4, col5 = st.columns(2)
+            with col4:
+                st.write("Looked up values.")
+                st.code("\n".join(list_lookup_ids))
+            with col5:
+                list_combined = [
+                    f"{name},{id}"
+                    for name, id in zip(list_lookup_names, list_lookup_ids)
+                ]
+                st.write("Names and looked up values.")
+                st.code("\n".join(list_combined))
 
 
 if __name__ == "__main__":
